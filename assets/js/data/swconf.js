0aa37054e57ccfabdbe3485532929a7b34259fab
RafaelRodriguest <RafaelRodriguest@users.noreply.github.com>
2025-10-15 23:32:53 +0000

    deploy: 525d76c6f6b0fdb5ee73567f3fdcf48171eae72e

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1760571172',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/CATEGORIAS/',
+      
+        '/tags/',
+      
+        '/ARQUIVOS/',
+      
+        '/SOBRE-MIM/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
